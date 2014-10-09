c65674f3f6620790e63075e029d1425e930d390d
Jack Yen <jackyen@gmail.com>
2014-10-08 22:40:49 -0400

    test

---
+++ src/pythonPractice.py
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="chiyuyen"
+__date__ ="$Oct 8, 2014 9:44:18 PM$"
+
+
+
+ 
+
+if __name__ == "__main__":
+    print "Hello World"
